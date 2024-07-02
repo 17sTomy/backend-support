13ec65efa640fdb93fa3c37fb106184670c65407
nicochavez <nicolaschavez530@gmail.com>
2024-07-02 19:01:53 -0300

    add delete-summary-result

--- backend/AI/views.py
+++ backend/AI/views.py
@@ -7,6 +7,7 @@ from .serializers import (
 from db import get_db
 from rest_framework.decorators import api_view
 from utils import run_ai
+from django.views.decorators.csrf import csrf_exempt
 
 
 class EmotionAnalysisView(APIView):
@@ -37,5 +38,26 @@ def get_summary_results(request):
         return Response(
             documents, status=status.HTTP_200_OK, content_type="application/json"
         )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@csrf_exempt
+@api_view(["DELETE"])
+def delete_summary_result(request):
+    db = get_db()
+    collection = db["summary_results"]
+    
+
+    summary_id = request.data.get("_id")
+    
+    if not summary_id:
+        return Response({"error": "Missing '_id' in request data"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        result = collection.delete_one({"_id": summary_id})
+        if result.deleted_count == 1:
+            return Response("Deleted", status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Document not found"}, status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
