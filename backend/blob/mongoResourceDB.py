e38673946c4c750a438581e31fb6f29b08b7b48e
Leonardo Avendaño <leonardo.a@oppizi.com>
2024-07-02 18:37:52 -0300

    Fix mongo conection

--- backend/blob/mongoResourceDB.py
+++ backend/blob/mongoResourceDB.py
@@ -1,46 +1,45 @@
 from pymongo import MongoClient
 from datetime import datetime
+from django.conf import settings
 
 
-class MongoCommands():
+class MongoCommands:
     def __init__(self) -> None:
-        self.client = MongoClient('mongodb+srv://support:JPB2v7xa1rwGmjS8@cluster0.rxj5qey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-        self.db = self.client['support']
-    
+        self.client = MongoClient(settings.MONGODB_URL)
+        self.db = self.client["support"]
+
     def get_db(self):
-        return self.client['support']
-    
+        return self.client["support"]
+
     def insert_into_db(self, file_name, audio_url):
         actual_fecha = datetime.now()
         fecha = actual_fecha.date()
         fecha = fecha.isoformat()
-        calls_collection = self.db['calls']
-        
+        calls_collection = self.db["calls"]
+
         call_document = {
-            'filename': file_name,
-            'url': audio_url,
-            'date': fecha,
-            'estado': 'Pendiente'
+            "filename": file_name,
+            "url": audio_url,
+            "date": fecha,
+            "estado": "Pendiente",
         }
         calls_collection.insert_one(call_document)
-    
+
     def get_all_calls(self):
-        calls_collection = self.db['calls']
+        calls_collection = self.db["calls"]
         return list(calls_collection.find())
-    
+
     def get_all_detailed_results(self):
-        detailed_result_collection = self.db['detailed_result']
+        detailed_result_collection = self.db["detailed_result"]
         return list(detailed_result_collection.find())
-    
-    def get_all_summary_results(self):  
-        self.db = self.client['support']
-        summary_result_collection = self.db['summary_result']
+
+    def get_all_summary_results(self):
+        self.db = self.client["support"]
+        summary_result_collection = self.db["summary_result"]
         documents = list(summary_result_collection.find())
         for doc in documents:
             doc["_id"] = str(doc["_id"])
         return documents
-
-    
 
 
 a = MongoCommands()
